80cf8c831e71424186a97e55493e3b768c04f887
Jacob Ayinsakia <jacob.ayinsakia@hubtel.com>
2025-04-07 21:05:35 +0000

    Show stations table on the homepage

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
+stations = pd.read_csv("data-small/stations.txt", skiprows=17)
+stations = stations[["STAID","STANAME                                 "]]
+# df = pd.read_csv()
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
+
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
+    filename = "data-small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
